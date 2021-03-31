0ad733b6f6e922e46d0bc7f18738b6bcd184b3bd
Keith Mellea <keithmellea@gmail.com>
2021-03-31 17:17:53 -0400

    working on phase 3

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,9 +2,34 @@ const express = require("express");
 const router = express.Router();
 const db = require('../db/models');
 const { Tweet } = db;
+const { check, validationResult } = require('express-validator');
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
+const validateTweet = [
+    check('message')
+    .exists({ checkFalsy: true })
+    .withMessage('Tweet can\'t be empty')
+    .isLength( { max: 280 })
+    .withMessage('Tweet can\'t be longer than 280 characters')
+];
+
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+
+        const errors = validationErrors.array().map((error) => error.msg);
+
+        const err = Error("Bad request.");
+        err.errors = errors;
+        err.status = 400;
+        err.title = "Bad request.";
+        return next(err);
+    }
+
+    next();
+}
 // GET /tweets
 router.get("/", asyncHandler( async (req, res) => {
     const tweets = await Tweet.findAll();
@@ -29,6 +54,11 @@ router.get("/:id(\\d+)", asyncHandler( async (req, res, next) => {
     };
 }));
 
+router.post("/", validateTweet, handleValidationErrors, asyncHandler( async (req, res) => {
+    const { message } = req.body;
+    const newTweet = await Tweet.create( { message });
+    res.status(201).json( { newTweet });
 
+}))
 
 module.exports = router;
