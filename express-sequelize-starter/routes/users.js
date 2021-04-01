1ddd86dc65d7ef918608d921b99b6070ae24622d
Keith Mellea <keithmellea@gmail.com>
2021-04-01 17:03:42 -0400

    commmit

--- express-sequelize-starter/routes/users.js
+++ express-sequelize-starter/routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { check } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const { asyncHandler, handleValidationErrors } = require("../utils");
-
+const db = require("../db/models");
+const { User } = db;
+const { getuserToken } = require('../auth.js');
 const validateUsername = [
     check("username")
         .exists({ checkFalsy: true })
@@ -23,7 +25,14 @@ const validateEmailAndPassword = [
 router.post("/", validateUsername, validateEmailAndPassword, handleValidationErrors, asyncHandler(async ( req, res) => {
     const { username, password, email } = req.body;
     const hashedPW = await bcrypt.hash(password, 10);
-   
-}));
+   const user = await User.create({ email, hashedPW, username })
+
+   const token = getUserToken(user);
+   res.status(201).json({
+       user: { id: user.id },
+       token,
+   });
+})
+);
 
 module.exports = router;
